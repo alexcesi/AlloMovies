a4ebe7cbdf78d89ab50c610ede33f946080561a9
alexcesi <alexvesier@gmail.com>
2023-05-10 15:57:36 +0200

    first commist

---
+++ app/src/main/java/network/ApiService.java
@@ -0,0 +1,29 @@
+package network;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiService {
+    private static final String BASE_URL = "https://api.themoviedb.org/3/";
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            // Ajouter l'intercepteur de journalisation pour afficher les logs des requêtes et réponses
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+            httpClientBuilder.addInterceptor(loggingInterceptor);
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClientBuilder.build())
+                    .build();
+        }
+        return retrofit;
+    }
+}
